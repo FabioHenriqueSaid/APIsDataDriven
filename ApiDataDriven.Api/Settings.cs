81f6bd04fcccb0afc8b833c4f4d273e039160a2c
fabiohenriquesaid <fabiosaid635@gmail.com>
2021-02-14 20:40:12 -0300

    Implementação JWT

--- ApiDataDriven.Api/Settings.cs
+++ ApiDataDriven.Api/Settings.cs
@@ -1,4 +1,4 @@
-namespace Backoffice
+namespace Api
 {
     public static class Settings
     {
--- ApiDataDriven.Api/Startup.cs
+++ ApiDataDriven.Api/Startup.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using System.Text;
 using ApiDataDriven.Api.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
@@ -8,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api
 {
@@ -31,6 +32,26 @@ namespace Api
             });
             // services.AddResponseCaching();
             services.AddControllers();
+
+
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
 
             //services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("connectionString"));
             services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
